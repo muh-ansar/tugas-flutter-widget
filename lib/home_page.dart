e6a52e87fecda708d0430d94da4675bcff2eaec8
muh-ansar <113844496+muh-ansar@users.noreply.github.com>
2024-03-20 23:23:09 +0800

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -79,3 +79,29 @@ class BaseLayout extends StatelessWidget{
     );
   }
 }
+
+
+Widget _buildStack() {
+  return Stack(
+    alignment: const Alignment(0.6, 0.6),
+    children: [
+      const CircleAvatar(
+        backgroundImage: AssetImage('images/wobafet.jpg'),
+        radius: 100,
+      ),
+      Container(
+        decoration: const BoxDecoration(
+          color: Colors.black45,
+        ),
+        child: const Text(
+          'Mia B',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    ],
+  );
+}
